965a535da2bf205ccd7332e3d4490de3b9831e92
Lucile <ldauriac@etu.u-bordeaux.fr>
2020-04-09 15:42:08 +0200

    fonction de base

--- Application/Behavior/Shape.java
+++ Application/Behavior/Shape.java
@@ -7,4 +7,18 @@ public interface Shape {
     public void mouv(double x, double y);
 
     public void mouv(Point p);
+    
+    public void roundedCorners();
+    
+    public void leftRotation();
+    
+    public void rightRotation();
+    
+    public void display();
+    
+    public void color(Color color);
+    
+    public void reduce(int num);
+    
+    public void extend(int num);
 }
